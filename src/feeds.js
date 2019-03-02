2d73fe575f3ebdd652ae6f96b7b22bf770978dba
Jimmy McBroom <jmcbroom@posteo.net>
2019-03-02 08:39:08 -0500

    upgrade to 16.8; useState hook!

--- src/feeds.js
+++ src/feeds.js
@@ -1,49 +1,49 @@
 const feeds = [
-    {
-        name: "ddot",
-        services: {
-            "1": "weekday",
-            "2": "saturday",
-            "3": "sunday"
-        }
-    },
-    {
-        name: "smart",
-        services: {
-            "2": "weekday",
-            "3": "saturday",
-            "6": "sunday"
-        }
-    },
-    {
-        name: "the-ride",
-        services: {
-            "3_merged_287881": "weekday",
-            "3_merged_287885": "weekday",
-            "1_merged_287879": "saturday",
-            "1_merged_287883": "saturday",
-            "1_merged_301738": "saturday",
-            "1_merged_301742": "saturday",
-            "2_merged_301741": "sunday",
-            "2_merged_301745": "sunday",
-            "3_merged_301740": "weekday",
-            "3_merged_301744": "weekday",
-            "2_merged_287882": "sunday",
-            "2_merged_287886": "sunday"
-        }
-    },
-    {
-        name: "transit-windsor",
-        services: {
-            "7": "weekday",
-            "8": "weekday",
-            "9": "weekday",
-            "2": "weekday",
-            "3": "weekday",
-            "4": "saturday",
-            "6": "sunday"
-        }
-    },
+  {
+    name: "ddot",
+    services: {
+      "1": "weekday",
+      "2": "saturday",
+      "3": "sunday"
+    }
+  },
+  {
+    name: "smart",
+    services: {
+      "2": "weekday",
+      "3": "saturday",
+      "6": "sunday"
+    }
+  },
+  {
+    name: "the-ride",
+    services: {
+      "3_merged_287881": "weekday",
+      "3_merged_287885": "weekday",
+      "1_merged_287879": "saturday",
+      "1_merged_287883": "saturday",
+      "1_merged_301738": "saturday",
+      "1_merged_301742": "saturday",
+      "2_merged_301741": "sunday",
+      "2_merged_301745": "sunday",
+      "3_merged_301740": "weekday",
+      "3_merged_301744": "weekday",
+      "2_merged_287882": "sunday",
+      "2_merged_287886": "sunday"
+    }
+  },
+  {
+    name: "transit-windsor",
+    services: {
+      "7": "weekday",
+      // "8": "weekday",
+      // "9": "weekday",
+      // "2": "weekday",
+      // "3": "weekday",
+      "4": "saturday",
+      "6": "sunday"
+    }
+  }
 ];
 
 export default feeds;
--- src/templates/route-page.js
+++ src/templates/route-page.js
@@ -4,73 +4,8 @@ import Layout from "../components/layout";
 import RouteStops from "../components/RouteStops";
 import RouteMap from "../components/RouteMap";
 import { Tab } from "semantic-ui-react";
-import feeds from "../feeds";
-import _ from "lodash";
 import wkx from "wkx";
-
-const RouteTrips = ({ trips, feedIndex }) => {
-  let filteredTrips = _.filter(trips, t => {
-    return (
-      t.direction === 0 &&
-      feeds[feedIndex - 1].services[t.service] === "weekday"
-    );
-  }).sort((a, b) => {
-    return (
-      a.stopTimes[0].arrivalTime.hours * 60 +
-      a.stopTimes[0].arrivalTime.minutes -
-      (b.stopTimes[0].arrivalTime.hours * 60 +
-        b.stopTimes[0].arrivalTime.minutes)
-    );
-  });
-  let timepointList = filteredTrips
-    .sort((a, b) => {
-      return (
-        b.stopTimes.map(s => s.timepoint).reduce((acc, val) => acc + val) -
-        a.stopTimes.map(s => s.timepoint).reduce((acc, val) => acc + val)
-      );
-    })[0]
-    .stopTimes.filter(st => {
-      return st.timepoint === 1;
-    });
-  console.log(filteredTrips);
-  return (
-    <table>
-      <thead>
-        <tr>
-          {timepointList.map(t => (
-            <th key={t.stop.stopId}>{t.stop.stopName}</th>
-          ))}
-        </tr>
-      </thead>
-      <tbody>
-        {filteredTrips.map(tr => (
-          <tr key={tr.id}>
-            {timepointList.map((t, i) => {
-              let filtered = tr.stopTimes.filter(
-                st => st.stop.stopId === t.stop.stopId && st.timepoint === 1
-              );
-              if (filtered.length === 0) {
-                return <td>-</td>;
-              } else {
-                let st = filtered[0];
-                return (
-                  <td key={st.stop.stopId}>
-                    {st.arrivalTime.hours} {st.arrivalTime.minutes}
-                  </td>
-                );
-              }
-            })}
-          </tr>
-        ))}
-      </tbody>
-    </table>
-    // <ul>
-    //   {filteredTrips.map(t => (
-    //     <li>{t.id} {feeds[feedIndex - 1].services[t.service]} {t.direction}</li>
-    //   ))}
-    // </ul>
-  );
-};
+import { RouteSchedule } from "../components/RouteSchedule";
 
 export default ({ data, pageContext }) => {
   const r = data.postgres.route[0];
@@ -123,7 +58,7 @@ export default ({ data, pageContext }) => {
       menuItem: "Schedule",
       render: () => (
         <Tab.Pane>
-          <RouteTrips trips={r.trips} feedIndex={pageContext.feedIndex} />
+          <RouteSchedule trips={r.trips} feedIndex={pageContext.feedIndex} />
         </Tab.Pane>
       )
     },
