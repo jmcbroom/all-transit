a03cfdbed2d8934302e6eb50a651e73e97fee3dd
jessica <jessica.mcinchak@gmail.com>
2019-05-21 19:06:10 -0400

    page two sandbox

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import { Button, Grid, Segment, Header, Placeholder, List, Icon } from 'semantic-ui-react'
 
 import Layout from '../components/layout'
 import SEO from '../components/seo'
@@ -7,9 +8,116 @@ import SEO from '../components/seo'
 const SecondPage = () => (
   <Layout>
     <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
+    <Header as='h1'>Detroit Transit Guide</Header>
+    <Segment basic>
+      <Grid>
+        <Grid.Row centered>
+          <Button.Group size='big' fluid>
+            <Button color='yellow'>LEARN THE SYSTEM</Button>
+            <Button.Or />
+            <Button color='green'>I'M READY TO RIDE</Button>
+          </Button.Group>
+        </Grid.Row>
+        <Grid.Row columns={2} divided>
+          <Grid.Column>
+            <List>
+              <List.Item>
+                <List.Icon name='angle right' />
+                <List.Content>
+                  <List.Header as='a'>Basics of transit</List.Header>
+                  <List.Description>
+                    Perfect for first-time riders
+                  </List.Description>
+                </List.Content>
+              </List.Item>
+              <List.Item>
+                <List.Icon name='angle right' />
+                <List.Content>
+                  <List.Header as='a'>How to ride</List.Header>
+                  <List.Description>
+                    Know before you go
+                  </List.Description>
+                </List.Content>
+              </List.Item>
+              <List.Item>
+                <List.Icon name='angle right' />
+                <List.Content>
+                  <List.Header as='a'>Transit providers</List.Header>
+                  <List.Description>Who's running my bus?</List.Description>
+                </List.Content>
+              </List.Item>
+            </List>
+          </Grid.Column>
+          <Grid.Column>
+            <List>
+              <List.Item>
+                <List.Icon name='angle right' />
+                <List.Content>
+                  <List.Header as='a'>Use our trip planner</List.Header>
+                  <List.Description>
+                    Point-to-point, step-by-step directions
+                  </List.Description>
+                </List.Content>
+              </List.Item>
+              <List.Item>
+                <List.Icon name='angle right' />
+                <List.Content>
+                  <List.Header as='a'>Where's my bus?</List.Header>
+                  <List.Description>
+                    Access real-time info
+                  </List.Description>
+                </List.Content>
+              </List.Item>
+              <List.Item>
+                <List.Icon name='angle right' />
+                <List.Content>
+                  <List.Header as='a'>Fares and passes</List.Header>
+                  <List.Description>Options to pay for your ride</List.Description>
+                </List.Content>
+              </List.Item>
+              <List.Item>
+                <List.Icon name='angle right' />
+                <List.Content>
+                  <List.Header as='a'>Routes and stops</List.Header>
+                  <List.Description>Find service near you</List.Description>
+                </List.Content>
+              </List.Item>
+              <List.Item>
+                <List.Icon name='angle right' />
+                <List.Content>
+                  <List.Header as='a'>System map</List.Header>
+                  <List.Description>Get the big picture</List.Description>
+                </List.Content>
+              </List.Item>
+            </List>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    </Segment>
+    <Segment basic>
+      <Header as='h3'>COMMUNITY RESOURCES</Header>
+      <Placeholder>
+        <Placeholder.Paragraph>
+          <Placeholder.Line />
+          <Placeholder.Line />
+          <Placeholder.Line />
+          <Placeholder.Line />
+        </Placeholder.Paragraph>
+        <Placeholder.Paragraph>
+          <Placeholder.Line />
+          <Placeholder.Line />
+          <Placeholder.Line />
+          <Placeholder.Line />
+        </Placeholder.Paragraph>
+      </Placeholder>
+    </Segment>
+    <Segment basic>
+      <Grid centered>
+        <Grid.Row>
+          <Button size='big' centered color='purple'>GO FURTHER</Button>
+        </Grid.Row>
+      </Grid>
+    </Segment>
   </Layout>
 )
 
